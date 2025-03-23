e726ab5b3ddc2907ff0dd24cc8241829162fd0a9
Abhishek Bhattacharya <babhishek21@yahoo.co.in>
2025-03-23 04:54:24 +0100

    [fix] Revert improper import of version from package.json on svelte config.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import { version } from './package.json' with { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		version: {
-			name: version
+			name: pkg.version
 		}
 	}
 };
