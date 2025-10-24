e1e4060eb56d3ee4384aba2c5e301efec3ad59fb
Abi <babhishek21@yahoo.co.in>
2025-10-24 11:51:33 +0530

    Fix eslint; works on svelte files now.

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import svelteConfig from './svelte.config.js';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -13,22 +14,14 @@ export default [
 	js.configs.recommended,
 	...svelte.configs.recommended,
 	prettier,
-	...svelte.configs['flat/prettier'],
+	...svelte.configs.prettier,
 	{
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
+			globals: { ...globals.browser, ...globals.node }
 		}
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.js'],
-
-		languageOptions: {
-			parserOptions: {
-				svelteConfig
-			}
-		}
+		languageOptions: { parserOptions: { svelteConfig } }
 	}
 ];
