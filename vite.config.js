474d450a06f44866742402704993473a109138d4
Abhishek Bhattacharya <babhishek21@yahoo.co.in>
2026-04-29 23:41:27 +0530

    Remove chrome devtools support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(), devtoolsJson()]
+	plugins: [sveltekit(), tailwindcss()]
 });
