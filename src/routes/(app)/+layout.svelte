b4d14fbfb1c98c9a6445f22464ef28190524d5b2
Abhishek Bhattacharya <babhishek21@yahoo.co.in>
2026-05-20 10:37:28 +0530

    Move main layout to (app) routing group.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.css';
+	import '../../app.css';
 	import { browser } from '$app/environment';
 	import FooterNav from '$lib/components/nav/FooterNav.svelte';
 	import { createSettingsContext } from '$lib/state/settings.svelte';
